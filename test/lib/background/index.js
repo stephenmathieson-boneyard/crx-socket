0dcac6f8e1d35e773d686e945329fb87ed4c4b8f
Stephen Mathieson <smath23@gmail.com>
2014-03-26 20:41:25 -0400

    test: log every event the server fires

--- test/lib/background/index.js
+++ test/lib/background/index.js
@@ -4,6 +4,18 @@ var Server = socket.Server;
 
 var server = new Server('background');
 
+server.on('connection', function (port) {
+  console.log('%s connected', port.name);
+});
+
+server.on('disconnect', function (port) {
+  console.log('%s disconnected', port.name);
+});
+
+server.on('message', function (msg) {
+  console.log('message', msg);
+});
+
 server.on('foo', function (data, res) {
   res.send('hello from background!');
 });
